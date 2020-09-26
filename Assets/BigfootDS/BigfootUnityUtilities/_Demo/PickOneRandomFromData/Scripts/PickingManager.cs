7706fe6e0e6a44e5e2ec124068fa612f586c7076
Alex H <alex.holder@bigfootds.com>
2020-09-26 17:09:54 +1000

    Example utility setup done

---
+++ Assets/BigfootDS/BigfootUnityUtilities/_Demo/PickOneRandomFromData/Scripts/PickingManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickingManager : MonoBehaviour
+{
+    public Text textCoinFlipResult;
+    public Text textD6RollResult;
+
+    private void Start()
+    {
+        RepickBoth();
+    }
+
+    [ContextMenu("Repick Both")]
+    public void RepickBoth()
+    {
+        RepickCoinflip();
+        RepickDiceRoll();
+    }
+
+    [ContextMenu("Repick Coin Flip")]
+    public void RepickCoinflip()
+    {
+        textCoinFlipResult.text = RandomPickFromData.PickRandomOneFromData<string>(
+            new string[] {
+                        "HEADS",
+                        "TAILS"
+            });
+    }
+
+    [ContextMenu("Repick Dice Roll")]
+    public void RepickDiceRoll()
+    {
+        textD6RollResult.text = RandomPickFromData.PickRandomOneFromData<int>(
+            new int[] {
+                        1,2,3,4,5,6
+            }).ToString();
+    }
+
+}
