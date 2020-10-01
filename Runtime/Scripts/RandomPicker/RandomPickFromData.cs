1c45a0126e046124e5463aa548c92cc76b1ff35b
AlexHolderDeveloper <alex.holder@bigfootds.com>
2020-10-01 06:22:56 +0000

    Update upm to output generated at 46fe6d7

--- Runtime/Scripts/RandomPicker/RandomPickFromData.cs
+++ Runtime/Scripts/RandomPicker/RandomPickFromData.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 public class RandomPickFromData
 {
@@ -6,4 +8,35 @@ public class RandomPickFromData
     {
         return incomingData[new Random().Next(incomingData.Length)];
     }
+
+    public static T PickRandomOneFromData<T>(List<T> incomingData)
+    {
+        return incomingData[new Random().Next(incomingData.Count)];
+    }
+
+    public static T PickRandomOneFromData<T>(Queue<T> incomingData)
+    {
+        return PickRandomOneFromData(incomingData.ToArray());
+    }
+
+    public static T PickRandomOneFromData<T>(Stack<T> incomingData)
+    {
+        return PickRandomOneFromData(incomingData.ToArray());
+    }
+
+    public static T PickRandomOneFromData<T>(ObservableCollection<T> incomingData)
+    {
+        T[] tempArray = new T[0];
+        incomingData.CopyTo(tempArray, 0);
+        return PickRandomOneFromData(tempArray);
+    }
+
+    public static T PickRandomOneFromData<T>(LinkedList<T> incomingData)
+    {
+        T[] tempArray = new T[0];
+        incomingData.CopyTo(tempArray, 0);
+        return PickRandomOneFromData(tempArray);
+    }
+
+
 }
